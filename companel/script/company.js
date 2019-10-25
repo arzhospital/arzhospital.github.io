671f769468a158b4ececfca52a0969c82c2fc3e7
Fadi Nammour <fadi.nammour@arzhospital.com>
2019-10-25 23:59:30 +0300

    companel

---
+++ companel/script/company.js
@@ -0,0 +1,21 @@
+var company = {
+    Code: "COM/panel",
+    Required: ["Underscore", "Moment", "ZingChart", "DataTables"],
+    Responsive: true,
+    Name: "Analytics Portal",
+    library: "",
+    GACode: 'UA-148842067-1',
+    OnPageLoad: () => {
+        new window.Main().OnPageLoad();
+    },
+};
+
+/* for web version */
+if (window.location.href.indexOf("/nammour.com/") <= -1) {
+    company.Store = "store/companel/";
+}
+
+/* for mobile version */
+// company.Store = "http://www.nammour.com/store/";
+
+var CMS_ROOT = "Hosted/" + company.Code + "/";
---
+++ companel/templates/d/dashboard.js
@@ -0,0 +1,49 @@
+window.__dashboard = class {
+    async main() {
+        let dbes = await window.sr._("CorporateMeasures.comDashboardEntryFindall", null, {
+            Enabled: true,
+            Dashboard: {
+                Enabled: true,
+                User: window.me
+            }
+        });
+        for (var i = 0; i < dbes.length; i++) {
+            var de = dbes[i];
+            var interval = {
+                Year: 1
+            };
+            if (de.Handler == "_dbNumber" || de.Handler == "_dbTable") {
+                interval = {
+                    Year: 1
+                };
+            } else if (de.Handler == "_dbLineChart" || de.Handler == "_dbSparkBars" || de.Handler == "_dbSparkLines" || de.Handler == "_dbNumberChart") {
+                interval = {
+                    Month: 1
+                };
+            }
+
+            var rs = new ReportSection(2014, company.Code, interval);
+            rs._start = moment().add(-1, 'y').startOf('year');
+            rs._end = moment().add(-1, 'y').endOf('year');
+            de.Data = await rs._run(de.GroupView.Name);
+        }
+        window.dasbhbaordEntries = sr.groupBy(dbes, "Dashboard");
+        await window._FrEMD.end();
+        var s = new ReportSection(2014, company.Code);
+        typeof(ga) !== "undefined" ? ga('send', {
+            hitType: 'event',
+            eventCategory: 'CorporateMeasures',
+            eventAction: 'Dashboard',
+            eventLabel: company.Code
+        }): null;
+
+        $.each($.grep(dbes, v => v.Handler == "_dbLineChart"), (_, v) => {
+            zingchart.render({
+                id: "_chart" + v.Id,
+                data: s._asZingOptions(s._data(v.Data, 'column', v.Name)),
+                height: '300',
+                width: '80%',
+            });
+        });
+    }
+}
