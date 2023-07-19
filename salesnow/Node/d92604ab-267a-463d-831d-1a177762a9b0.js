64b67acf515c00ccf33ee186135c409675dc6f56
arzhospital <46606950+arzhospital@users.noreply.github.com>
2023-07-19 07:34:54 +0200

    deployed by _github()

---
+++ salesnow/Node/d92604ab-267a-463d-831d-1a177762a9b0.js
@@ -0,0 +1,43 @@
+{
+	"__id": "d92604ab-267a-463d-831d-1a177762a9b0",
+	"address": null,
+	"port": 3000,
+	"online": "2023-07-19T05:34:49.283Z",
+	"secure": false,
+	"active": true,
+	"enabled": true,
+	"code": 1.3684792593505968,
+	"date": "2023-07-19T05:34:07.473Z",
+	"parent": {
+		"__id": "8385b4e6-242a-4f30-b09d-0b8aea58ef59",
+		"address": "",
+		"OPERATORS": {
+			"address": "!=",
+			"port": "!=",
+			"online": ">=",
+			"code": "!="
+		},
+		"port": "",
+		"online": "2023-07-19T05:34:19.080Z",
+		"active": true,
+		"enabled": true,
+		"code": 1.3684792593505968,
+		"date": "2023-07-19T05:34:07.881Z",
+		"parent": null,
+		"type": {
+			"__id": "f2b847f3-9389-4443-8ebf-36e5d1e4389d",
+			"active": true,
+			"enabled": true,
+			"date": "2023-07-19T05:34:08.320Z",
+			"name": "NodeJS"
+		}
+	},
+	"type": {
+		"__id": "9e0085a8-e948-40ad-bfe5-23b1f3aa2f8a",
+		"dynamic": true,
+		"active": true,
+		"enabled": true,
+		"date": "2023-07-19T05:34:10.875Z",
+		"name": "Browser"
+	}
+}
