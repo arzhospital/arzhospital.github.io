3eb85237334f32ed817df0c90302e133fe2ab6d9
arzhospital <46606950+arzhospital@users.noreply.github.com>
2023-07-19 07:33:24 +0200

    deployed by _github()

---
+++ salesnow/Node/a145da6b-eca1-42d0-9fdb-ee271aae2ab5.js
@@ -0,0 +1,24 @@
+{
+	"__id": "a145da6b-eca1-42d0-9fdb-ee271aae2ab5",
+	"address": "",
+	"OPERATORS": {
+		"address": "!=",
+		"port": "!=",
+		"online": ">=",
+		"code": "!="
+	},
+	"port": "",
+	"online": "2023-07-19T05:32:51.431Z",
+	"active": true,
+	"enabled": true,
+	"code": 0.4214792295652403,
+	"date": "2023-07-19T05:32:40.220Z",
+	"parent": null,
+	"type": {
+		"__id": "f6362ed3-1ea5-426b-ba73-144aab023d0d",
+		"active": true,
+		"enabled": true,
+		"date": "2023-07-19T05:32:40.640Z",
+		"name": "NodeJS"
+	}
+}
