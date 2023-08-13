b9710b7e9458f7ae4a78976219750a03845f9e6b
arzhospital <46606950+arzhospital@users.noreply.github.com>
2023-08-13 13:18:27 +0200

    deployed by _github()

--- salesnow/User/123.js
+++ salesnow/User/123.js
@@ -3,7 +3,7 @@
 	"active": true,
 	"enabled": true,
 	"code": "tmp",
-	"date": "2023-08-13T09:52:33.382Z",
+	"date": "2023-08-13T11:18:26.973Z",
 	"name": "Temp User",
 	"remark": "\nquery AccountsAndCases {\n  uiapi {\n    query {\n      Account {\n        edges {\n          node {\n            Name { value }\n            Industry { value }\n          }\n        }\n      }\n      Case(where: { AccountId: { inq: { Account: {\n        and: [\n          { Name: { like: \"United%\" } },\n          { Industry: { eq: \"Energy\" } }\n        ]},\n          ApiName:\"Id\" } } }) {\n        edges {\n          node {\n            AccountId { value }\n            Priority { value }\n            Subject { value }\n          }\n        }\n      }\n    }\n  }\n}\n                "
 }
